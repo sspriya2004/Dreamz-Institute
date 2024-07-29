428cb62f2a5945ba50296f0cb8e1053121eceddf
sspriya2004 <sspriya29874@gmail.com>
2024-07-29 12:56:55 +0530

    first commit

---
+++ JS/AddTuitionFees.js
@@ -0,0 +1,6 @@
+var partiallypaid = document.getElementById("partiallyPaid");
+function displayBox(obj){
+    if(obj.value=='partiallyPaid'){
+        document.getElementsByClassName("block_display").style.display = "block";
+    }
+}
